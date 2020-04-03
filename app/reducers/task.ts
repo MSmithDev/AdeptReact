517e8c57dfa7f4da98f577620825cc43faf417a7
Matt Smith <mattdsmith228@gmail.com>
2020-04-03 17:56:56 -0400

    Remove array of objects from initial state

--- app/reducers/task.ts
+++ app/reducers/task.ts
@@ -1,61 +1,11 @@
 import { test } from '../actions/task';
 
-const initialState = [
-  {
-    id: 0,
-    type: 'Move Linear',
-    something: 1,
-    x: 12
-  },
-  {
-    id: 1,
-    type: 'Move Joint',
-    something: 3,
-    x: 15
-  },
-  {
-    id: 2,
-    type: 'IO',
-    something: 3,
-    x: 15
-  },
-  {
-    id: 3,
-    type: 'Move Joint',
-    something: 3,
-    x: 15
-  },
-  {
-    id: 4,
-    type: 'Move Joint',
-    something: 3,
-    x: 15
-  },
-  {
-    id: 5,
-    type: 'Move Joint',
-    something: 3,
-    x: 15
-  },
-  {
-    id: 6,
-    type: 'Move Joint',
-    something: 3,
-    x: 15
-  },
-  {
-    id: 7,
-    type: 'Move Liner',
-    something: 3,
-    x: 15
-  },
-  {
-    id: 8,
-    type: 'Move Linear',
-    something: 3,
-    x: 15
-  }
-];
+const initialState = {
+  id: 0,
+  type: 'Move Linear',
+  something: 1,
+  x: 12
+};
 
 const taskReducer = (state = initialState, action: { type: string }) => {
   switch (action.type) {
